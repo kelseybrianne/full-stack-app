be6fc5712f55b33aa7219d5da31af16482c98304
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-16 22:57:54 -0800

    add dropdown menu to profile icon

---
+++ public/js/header.js
@@ -0,0 +1,21 @@
+// Check for click events on the navbar burger icon
+const profileIcon = document.querySelector("#profile-icon");
+const dropdownMenu = $("#dropdown-menu");
+
+const renderDropdown = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  if (dropdownMenu.hasClass("hidden")) {
+    dropdownMenu.removeClass("hidden");
+  } else {
+    dropdownMenu.addClass("hidden");
+  }
+
+  document.addEventListener("click", (event) => {
+    event.preventDefault();
+    dropdownMenu.addClass("hidden");
+  });
+};
+
+profileIcon.addEventListener("click", renderDropdown);
