9b2175b5785bc247290df7dc3600f3e5c8c6d4b1
maddy <maddykimborowicz@gmail.com>
2022-02-16 21:09:03 -0800

    login functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,12 +20,12 @@ router.post('/', async (req, res) => {
 // post login(/login)
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -46,6 +46,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
+    console.log(err)
     res.status(400).json(err);
   }
 });
