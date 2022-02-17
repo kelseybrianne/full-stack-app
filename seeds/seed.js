cc60f9251491a8606da94ac8da3f5740b873b240
Bertokeys1 <abarrientos93.ab@gmail.com>
2022-02-16 18:37:54 -0800

    updated seed files

---
+++ seeds/seed.js
@@ -0,0 +1,35 @@
+
+const sequelize = require('../config/connection');
+const { User, Post, Challenge, UserChallenge,  } = require('../models');
+
+const userData = require('./userData.json');
+const userChallengeData = require('./userChallengeData.json');
+const challengeData = require('./challengeData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await UserChallenge.bulkCreate(userChallengeData, {
+    returning: true,
+  })
+
+  await Challenge.bulkCreate(challengeData, {
+    returning: true,
+  })
+
+  await Post.bulkCreate(postData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+
+
+seedDatabase();
