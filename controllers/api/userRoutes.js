442d3fb4285344cd1b10547f77c0e1c513e48c3a
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-18 14:29:46 -0800

    create frontend for create challenge with a button icon in the header and a popup modal

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -13,9 +13,8 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(400).json(err);
-    
   }
 });
 
