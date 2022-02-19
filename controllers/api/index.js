00c5df46961e08be1519f218214d7336cebba487
Marshall Baccari <mbaccari@uw.edu>
2022-02-19 12:57:04 -0800

    made join button add the challenge to the users challenges

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const userChallengeRoutes = require('./userChallengeRoutes')
 
 router.use('/users', userRoutes);
+router.use('/userChallenge', userChallengeRoutes); 
 
 module.exports = router;
