1d9b02f991927235607044c2bb02d1b20dbf3d4c
maddy <maddykimborowicz@gmail.com>
2022-02-19 13:25:49 -0800

    troubleshooting datepicker/null

--- public/js/createChallenge.js
+++ public/js/createChallenge.js
@@ -1,18 +1,19 @@
-const postChallengeBtn = document.querySelector("#post-challenge-btn")
-
 const createChallenge = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
     const title = document.querySelector('#challenge-title').value.trim();
     const description = document.querySelector('#description').value.trim();
-    const endDate = document.querySelector('#description').value.trim();
+    const endDate = document.querySelector('#datepicker').value.trim();
   
-    if (title && description) {
+    console.log(title, description, endDate);
+
+    if (title && description && endDate) {
       // Send a POST request to the API endpoint
+    
       const response = await fetch('/api/challenge', {
         method: 'POST',
-        body: JSON.stringify({ title, description }),
+        body: JSON.stringify({ title, description, endDate }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -25,4 +26,4 @@ const createChallenge = async (event) => {
     }
   };
 
-  postChallengeBtn.addEventListener('submit', createChallenge);
+  document.querySelector("#post-challenge-btn").addEventListener('submit', createChallenge);
