4b6b66188885dc9527ce384dce167c8c2b25c99b
Kelsey Mailau <kelseymailau@gmail.com>
2022-03-02 18:01:31 -0800

    add time limit to cookies

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+      maxAge: 86400000
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
